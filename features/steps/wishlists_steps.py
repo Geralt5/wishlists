713f720e655f5251e7a3bf1d4e1363871d4f3ac0
James-Studio <hongjames111@gmail.com>
2024-12-04 03:01:25 +0000

    Add prefix for URLs used in BDD and JS

--- features/steps/wishlists_steps.py
+++ features/steps/wishlists_steps.py
@@ -40,7 +40,7 @@ def step_impl(context):
     """Delete all Wishlists and load new ones"""
 
     # Get a list all of the wishlists
-    rest_endpoint = f"{context.base_url}/wishlists"
+    rest_endpoint = f"{context.base_url}/api/wishlists"
     context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
     expect(context.resp.status_code).equal_to(HTTP_200_OK)
 
